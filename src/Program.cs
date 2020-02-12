048260645391421ea5616d02e8cd6dc27e71c58d
Kris Hale <karissa.hale72@gmail.com>
2020-02-11 19:33:55 -0500

    Completed step 1 of challenge 2

--- src/Program.cs
+++ src/Program.cs
@@ -7,49 +7,80 @@ namespace KrisHale.CodeLou.ExerciseProject
     {
         static void Main(string[] args)
         {
+            //     var inputtingStudent = true;
+            //     while (inputtingStudent)
+            //     {
+            //         InputStudent();
+
+            //         Console.WriteLine("Do you want to enter another student? (Y/N)");
+            //         inputtingStudent = Console.ReadLine().ToLower() == "y";
+            //     }
+
+            // }
+
+
+            // static Student InputStudent()
+            // {
+            //     Console.WriteLine("Enter Student Id");
+            //     var studentId = Convert.ToInt32(Console.ReadLine());
+            //     Console.WriteLine("Enter First Name");
+            //     var studentFirstName = Console.ReadLine();
+            //     Console.WriteLine("Enter Last Name");
+            //     var studentLastName = Console.ReadLine();
+            //     Console.WriteLine("Enter Class Name");
+            //     var className = Console.ReadLine();
+            //     Console.WriteLine("Enter Last Class Completed");
+            //     var lastClass = Console.ReadLine();
+            //     Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
+            //     var lastCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
+            //     Console.WriteLine("Enter Start Date in format MM/dd/YYYY");
+            //     var startDate = DateTimeOffset.Parse(Console.ReadLine());
+
+            //     var studentRecord = new Student();
+            //     studentRecord.StudentId = studentId;
+            //     studentRecord.FirstName = studentFirstName;
+            //     studentRecord.LastName = studentLastName;
+            //     studentRecord.ClassName = className;
+            //     studentRecord.StartDate = startDate;
+            //     studentRecord.LastClassCompleted = lastClass;
+            //     studentRecord.LastClassCompletedOn = lastCompletedOn;
+            //     Console.WriteLine($"Student Id | Name |  Class "); ;
+            //     Console.WriteLine($"{studentRecord.StudentId} | {studentRecord.FirstName} {studentRecord.LastName} | {studentRecord.ClassName} ");
+            //     Console.ReadKey();
+            //     return studentRecord;
+
             var inputtingStudent = true;
             while (inputtingStudent)
             {
-                InputStudent();
-
-                Console.WriteLine("Do you want to enter another student? (Y/N)");
-                inputtingStudent = Console.ReadLine().ToLower() == "y";
+                DisplayMenu();
+                var option = Console.ReadLine();
+                switch (int.Parse(option))
+                {
+                    case 1:
+                        InputStudent();
+                        break;
+                    case 2:
+                        DisplayStudents();
+                        break;
+                    case 3:
+                        SearchStudents();
+                        break;
+                    case 4:
+                        inputtingStudent = false;
+                        break;
+                }
+                
             }
-            
+
         }
-
-
-        static Student InputStudent()
-        {
-            Console.WriteLine("Enter Student Id");
-            var studentId = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter First Name");
-            var studentFirstName = Console.ReadLine();
-            Console.WriteLine("Enter Last Name");
-            var studentLastName = Console.ReadLine();
-            Console.WriteLine("Enter Class Name");
-            var className = Console.ReadLine();
-            Console.WriteLine("Enter Last Class Completed");
-            var lastClass = Console.ReadLine();
-            Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
-            var lastCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Start Date in format MM/dd/YYYY");
-            var startDate = DateTimeOffset.Parse(Console.ReadLine());
-
-            var studentRecord = new Student();
-            studentRecord.StudentId = studentId;
-            studentRecord.FirstName = studentFirstName;
-            studentRecord.LastName = studentLastName;
-            studentRecord.ClassName = className;
-            studentRecord.StartDate = startDate;
-            studentRecord.LastClassCompleted = lastClass;
-            studentRecord.LastClassCompletedOn = lastCompletedOn;
-            Console.WriteLine($"Student Id | Name |  Class "); ;
-            Console.WriteLine($"{studentRecord.StudentId} | {studentRecord.FirstName} {studentRecord.LastName} | {studentRecord.ClassName} ");
-            Console.ReadKey();
-            return studentRecord;
-        }
-    
+        private static void DisplayMenu()
+                {
+                    Console.WriteLine("Select from the following operations:");
+                    Console.WriteLine("1: Enter new student");
+                    Console.WriteLine("2: List all students");
+                    Console.WriteLine("3: Search for student by name");
+                    Console.WriteLine("4: Exit");
+                }
     }
 }
 
